8622ec3abde54a1816c78dc239439d176dcb602e
Deveshb15 <deveshb583@gmail.com>
2022-09-10 23:22:08 +0530

    chore: vid upload

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const ffmpeg = createFFmpeg({ log: true })
 function App() {
 
   const [ready, setReady] = useState(false)
+  const [video, setVideo] = useState()
 
   const load = async() => {
     await ffmpeg.load()
@@ -15,9 +16,30 @@ function App() {
     load()
   }, [])
 
+  console.log(video)
+  // console.log(URL.createObjectURL(video))
+
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="flex justify-center items-center min-h-screen bg-black text-white">
+      {
+        ready ? (
+          <div>
+            {
+              video && (
+                <video 
+                  controls
+                  width="250px"
+                  src={URL.createObjectURL(video)}
+                />
+              )
+            }
+
+            <input type="file" onChange={(e) => setVideo(e.target?.files?.item(0))} />
+          </div>
+        ) : (
+          <h1 className="text-3xl font-bold">Loading...</h1>
+        )
+      }
     </div>
   );
 }
