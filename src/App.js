fa4234936f9313bbbade2a1514267dbc76d4ca73
Deveshb15 <deveshb583@gmail.com>
2022-09-10 23:58:35 +0530

    feat: upload video or add src, convert and give result using ffmpeg

--- src/App.js
+++ src/App.js
@@ -1,47 +1,85 @@
-import { useState, useEffect } from 'react';
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
-const ffmpeg = createFFmpeg({ log: true })
+import { useState, useEffect } from "react";
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
+	const [ready, setReady] = useState(false);
+	const [video, setVideo] = useState(null);
+	const [gif, setGif] = useState(null);
 
-  const [ready, setReady] = useState(false)
-  const [video, setVideo] = useState()
+	const load = async () => {
+		await ffmpeg.load();
+		setReady(true);
+	};
 
-  const load = async() => {
-    await ffmpeg.load()
-    setReady(true)
-  }
+	useEffect(() => {
+		load();
+	}, []);
 
-  useEffect(() => {
-    load()
-  }, [])
+	console.log(typeof video);
+	// console.log(URL.createObjectURL(video))
+	const convertToGif = async () => {
+		ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
 
-  console.log(video)
-  // console.log(URL.createObjectURL(video))
+		await ffmpeg.run(
+			"-i",
+			"test.mp4",
+			"-t",
+			"2.5s",
+			"-ss",
+			"2.0",
+			"-f",
+			"gif",
+			"out.gif"
+		);
 
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-black text-white">
-      {
-        ready ? (
-          <div>
-            {
-              video && (
-                <video 
-                  controls
-                  width="250px"
-                  src={URL.createObjectURL(video)}
-                />
-              )
-            }
+		const data = ffmpeg.FS("readFile", "out.gif");
 
-            <input type="file" onChange={(e) => setVideo(e.target?.files?.item(0))} />
-          </div>
-        ) : (
-          <h1 className="text-3xl font-bold">Loading...</h1>
-        )
-      }
-    </div>
-  );
+		const url = URL.createObjectURL(
+			new Blob([data.buffer], { type: "image/gif" })
+		);
+		setGif(url);
+	};
+
+	return (
+		<div className="flex justify-center items-center min-h-screen bg-black text-white">
+			{ready ? (
+				<div>
+					{video && (
+						<video
+							controls
+							width="250px"
+							src={
+								typeof video == "object" ? URL.createObjectURL(video) : video
+							}
+						/>
+					)}
+
+					<div className="flex flex-col justify-center items-center">
+						<input
+							type="file"
+							onChange={(e) => setVideo(e.target?.files?.item(0))}
+						/>
+						<p>or</p>
+						<input
+							className="text-black px-4 py-2"
+							type="text"
+              placeholder="Enter a video url"
+							onChange={(e) => setVideo(e.target?.value)}
+						/>
+					</div>
+
+					<h1 className="py-4 text-xl font-bold">Results: </h1>
+
+					{video && <button className="text-base py-2 px-4 rounded-lg font-bold bg-white text-black border hover:bg-black hover:text-white" onClick={convertToGif}>Convert to GIF</button>}
+
+					{gif && <img src={gif} width="250px" alt="gif" />}
+				</div>
+			) : (
+				<h1 className="text-3xl font-bold">Loading...</h1>
+			)}
+		</div>
+	);
 }
 
 export default App;
