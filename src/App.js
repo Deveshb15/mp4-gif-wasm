c027a96fcce628efa5be08f41b29887d026e2277
Deveshb15 <deveshb583@gmail.com>
2022-09-10 22:52:02 +0530

    fix: SharedArrayBuffer allowance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+const ffmpeg = createFFmpeg({ log: true })
 
 function App() {
+
+  const [ready, setReady] = useState(false)
+
+  const load = async() => {
+    await ffmpeg.load()
+    setReady(true)
+  }
+
+  useEffect(() => {
+    load()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
     </div>
   );
 }
